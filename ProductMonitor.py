22871f6e2f530107e28ec1ab84bcc7551291d3b3
Kyle Bellgraph <kbellgra@gmail.com>
2021-01-17 10:57:31 -0600

    Added rotating logs

--- ProductMonitor.py
+++ ProductMonitor.py
@@ -7,7 +7,9 @@ import glob
 import logging
 import shutil
 import random
+
 from twilio.rest import Client
+from logging.handlers import TimedRotatingFileHandler
 
 class Product:
     def __init__(self, productname, producturls, productnumbers):
@@ -35,13 +37,13 @@ class Product:
                                 "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36"
                                 ]
             user_agent = random.choice(top_user_agents)
-            logging.info(f'User Agent: {user_agent}')
+            logger.info(f'User Agent: {user_agent}')
             headers = {'User-Agent': user_agent}
 
             website = requests.get(current_url,headers=headers)
 
             if website.status_code != 200:
-                logging.error(f'Return Code {website.status_code} from {current_url}')
+                logger.error(f'Return Code {website.status_code} from {current_url}')
 
             if current_instock_string in website.text:
                 self.instockalert(current_site, current_url)
@@ -59,21 +61,6 @@ class Product:
         '''Legacy Method of disabling products '''
         f= open("trigger","w+")
         f.close()
-
-    '''class Website:
-    def __init__(self, storename, storeurl, productname, instockstring):
-        self.storename = storename
-        self.storeurl = storeurl
-        self.productname = productname
-        self.instockstring = instockstring
-
-    def checkstock(self):
-        ''Requests site, searches html and returns True if instockstring is found'
-        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:72.0) Gecko/20100101 Firefox/72.0'
-        headers = {'User-Agent': user_agent}
-        website = requests.get(self.storeurl,headers=headers)
-
-        return (self.instockstring in website.text)'''
 
 def SendTwilioMessage(account_sid, auth_token, source, destination, product, url):
     client = Client(account_sid, auth_token)
@@ -111,17 +98,23 @@ def DisableProduct(filename):
     source = os.getcwd() + filename
     destination = os.path.dirname(os.path.abspath(__file__)) + '/products/disabled/'
     shutil.move(filename, destination)
-    logging.info(f'Moving {filename} to {destination}')
-
+    logger.info(f'Moving {filename} to {destination}')
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    logging.basicConfig(level=logging.DEBUG,filename='log.txt', format='%(levelname)s - %(asctime)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S')
-    
+    #logging.basicConfig(level=logging.DEBUG,filename='log.txt', format='%(levelname)s - %(asctime)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S')
+    #SetupRotatingLog()
+
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.DEBUG)
+    handler = TimedRotatingFileHandler('log.txt', when='d',interval=1,backupCount=7)
+    logger.addHandler(handler)
+
+
     if os.path.isfile('trigger'):
         print ("STOPPING - Trigger File Exists")
-        logging.error("STOPPING - Trigger File Exists")
+        logger.error("STOPPING - Trigger File Exists")
         quit()
 
     products = OpenProductDir('/products')
@@ -129,17 +122,17 @@ if __name__ == "__main__":
     for filename in products:
         if ValidateJSON(filename):
             data = ImportJSON(filename)
-            logging.info(f'Imported {filename}')
+            logger.info(f'Imported {filename}')
             productname = data['Product']
             urls = data['URLs']
             numbers = data['Numbers']
             product = Product(productname, urls, numbers)
             result = product.checkstock()
-            logging.info(f'Result of CheckStock() for {productname}: {result}')
+            logger.info(f'Result of CheckStock() for {productname}: {result}')
 
             if result:
                 DisableProduct(filename)
 
         else:
-            logging.error(f'{filename} is not valid json format, quitting')
+            logger.error(f'{filename} is not valid json format, quitting')
             quit()
